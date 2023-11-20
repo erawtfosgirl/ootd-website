6b4ef82c6916290eab4355422aa9c89f4ccc6ee1
vjuggler <vefaferzeliyeva11@gmail.com>
2023-11-21 00:24:15 +0400

    change wishlist icon

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -37,14 +37,11 @@ exit.addEventListener('click', () => {
 let allwishlist = document.querySelectorAll('.wishlist');
 allwishlist.forEach(item => {
     item.addEventListener('click', () => {
-        let path = item.firstElementChild.firstElementChild;
-        let isActive = path.classList.contains('active');
-        if (!isActive) {
-            path.classList.add('active');
-        } else {
-            path.classList.remove('active');
-        }
+        let firstIcon = item.firstElementChild;
+        let secondIcon = item.lastElementChild;
 
+        firstIcon.classList.toggle('hidden');
+        secondIcon.classList.toggle('hidden');
     })
 })
 

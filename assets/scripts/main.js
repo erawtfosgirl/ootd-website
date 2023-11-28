8ca06583958582c5939f98853177bb4fe3223768
vjuggler <vefaferzeliyeva11@gmail.com>
2023-11-28 22:58:09 +0400

    shop page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -86,3 +86,62 @@ questions.forEach(question => {
 
 })
 
+
+/*Filter Shop Menu*/
+
+let shopfilterbtn=document.querySelector('.shopfilter-btn');
+let closefiltermenu=document.querySelector('.closefiltermenu');
+let filtermenu=document.querySelector('.filter-menu');
+let body=document.querySelector('body')
+
+shopfilterbtn.addEventListener('click',()=>{
+    filtermenu.classList.add('active');
+})
+closefiltermenu.addEventListener('click',()=>{
+    filtermenu.classList.remove('active');
+})
+
+/*Price Range*/
+
+const rangeInput = document.querySelectorAll(".range-input input"),
+    priceInput = document.querySelectorAll(".price-input input"),
+    range = document.querySelector(".slider .progress");
+let priceGap = 1000;
+
+priceInput.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        let minPrice = parseInt(priceInput[0].value),
+            maxPrice = parseInt(priceInput[1].value);
+
+        if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+            if (e.target.className === "input-min") {
+                rangeInput[0].value = minPrice;
+                range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+            } else {
+                rangeInput[1].value = maxPrice;
+                range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    });
+});
+
+rangeInput.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value);
+
+        if (maxVal - minVal < priceGap) {
+            if (e.target.className === "range-min") {
+                rangeInput[0].value = maxVal - priceGap;
+            } else {
+                rangeInput[1].value = minVal + priceGap;
+            }
+        } else {
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
+
